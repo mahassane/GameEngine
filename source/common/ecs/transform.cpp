eddea25b82d1c9ebd1137c68aa8a52245525ce7e
MariamEssam553 <ghanemmariam26@gmail.com>
2024-11-29 20:29:08 +0200

    Requirement 3 Transform

--- source/common/ecs/transform.cpp
+++ source/common/ecs/transform.cpp
@@ -10,7 +10,13 @@ namespace our {
     // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
     glm::mat4 Transform::toMat4() const {
         //TODO: (Req 3) Write this function
-        return glm::mat4(1.0f); 
+        glm::mat4 mat_T = glm::translate(glm::mat4(1.0f), position);
+        glm::mat4 mat_R = glm::yawPitchRoll(rotation.y, rotation.x, rotation.z);
+        glm::mat4 mat_S = glm::scale(glm::mat4(1.0f), scale);
+        glm::mat4 Transform_mat = mat_T * mat_R * mat_S;
+
+        return Transform_mat;
+        // return glm::mat4(1.0f);
     }
 
      // Deserializes the entity data and components from a json object
