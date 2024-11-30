4012f8a7a4a996f7e149b43772db81f559d75f95
mahmoudtaha0 <mahmoud.ahmed021@eng-st.cu.edu.eg>
2024-11-30 23:27:53 +0200

    [Requirement 11] Resolved

--- source/common/texture/texture-utils.cpp
+++ source/common/texture/texture-utils.cpp
@@ -8,7 +8,8 @@
 our::Texture2D* our::texture_utils::empty(GLenum format, glm::ivec2 size){
     our::Texture2D* texture = new our::Texture2D();
     //TODO: (Req 11) Finish this function to create an empty texture with the given size and format
-    texture->bind(); 
+    texture->bind();
+    glTexStorage2D(GL_TEXTURE_2D, 1, format, size.x, size.y);
 
     // Allocate texture storage
     glTexImage2D(GL_TEXTURE_2D, 0, format, size.x, size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
